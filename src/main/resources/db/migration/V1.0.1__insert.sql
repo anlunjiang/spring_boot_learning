72f0df3720399b6f1c66fd6bf05aedcd8b45188c
anlunjiang <anlunjiang@outlook.com>
2022-04-15 00:51:24 +0100

    change migration script to own and provide token via env var

--- src/main/resources/db/migration/V1.0.1__insert.sql
+++ src/main/resources/db/migration/V1.0.1__insert.sql
@@ -1,2 +1,2 @@
-insert into GITHUB_PROJECT(ORG_NAME, REPO_NAME) values ('spring-projects', 'spring-boot');
+insert into GITHUB_PROJECT(ORG_NAME, REPO_NAME) values ('anlunjiang', 'project_tyche');
 insert into GITHUB_PROJECT(ORG_NAME, REPO_NAME) values ('anlunjiang', 'spring_boot_learning');
